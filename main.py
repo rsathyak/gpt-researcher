3631550cec7b7b2d41d8396eff24141f2d9ed0a6
rsathyak <rsathyak@asu.edu>
2024-02-04 00:27:36 -0700

    reverted to old changes

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from backend.server import app
 from dotenv import load_dotenv
-from mangum import Mangum
 load_dotenv()
 
-handler = Mangum(app)
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
